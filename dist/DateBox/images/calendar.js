c8160497c8f96860b88f1aaf3baa99a0c2811012
Azaryan-A <armana@boomte.ch>
2021-09-25 17:47:27 +0400

    build

--- dist/DateBox/images/calendar.js
+++ dist/DateBox/images/calendar.js
@@ -5,15 +5,19 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-const Calendar = () => /*#__PURE__*/React.createElement("svg", {
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const Calendar = () => /*#__PURE__*/_react.default.createElement("svg", {
   width: "14",
   height: "16",
   viewBox: "0 0 24 26",
   xmlns: "http://www.w3.org/2000/svg"
-}, /*#__PURE__*/React.createElement("path", {
+}, /*#__PURE__*/_react.default.createElement("path", {
   d: "M22 2H18V0H16V2H8V0H6V2H2C0.9 2 0 2.9 0 4V24C0 25.1 0.9 26 2 26H22C23.1 26 24 25.1 24 24V4C24 2.9 23.1 2 22 2ZM22 24H2V10H22V24ZM22 8H2V4H6V6H8V4H16V6H18V4H22V8Z",
   fill: "#2B5672"
-}), /*#__PURE__*/React.createElement("rect", {
+}), /*#__PURE__*/_react.default.createElement("rect", {
   x: "17",
   y: "19",
   width: "3",
--- dist/DateBox/images/end.js
+++ dist/DateBox/images/end.js
@@ -5,12 +5,16 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-const End = () => /*#__PURE__*/React.createElement("svg", {
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const End = () => /*#__PURE__*/_react.default.createElement("svg", {
   width: "15",
   height: "15",
   viewBox: "0 0 20 20",
   xmlns: "http://www.w3.org/2000/svg"
-}, /*#__PURE__*/React.createElement("path", {
+}, /*#__PURE__*/_react.default.createElement("path", {
   d: "M9.99 0C4.47 0 0 4.48 0 10C0 15.52 4.47 20 9.99 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 9.99 0ZM10 18C5.58 18 2 14.42 2 10C2 5.58 5.58 2 10 2C14.42 2 18 5.58 18 10C18 14.42 14.42 18 10 18ZM10.5 5H9V11L14.25 14.15L15 12.92L10.5 10.25V5Z"
 }));
 
