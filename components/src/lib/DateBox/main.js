a875c757517e758a52d2843c6f07bf328f31d5cc
Zhenya Aleksanyan <zhenyaa@boomte.ch>
2024-03-25 12:00:18 +0400

    icons css improt added in datebox main js

--- components/src/lib/DateBox/main.js
+++ components/src/lib/DateBox/main.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import DateBoxComponent from "./dateBox/DateBox";
 import TimeBox from "./timeBox/TimeBox";
+import "../icons.css";
 
 const DateBox = ({
   start,
