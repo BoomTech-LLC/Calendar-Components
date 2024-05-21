d9f6dae1e224b10a381d3dd7eec936212a8a4e90
NarekBoomTech <nareks@boomte.ch>
2024-05-20 22:47:03 -1200

    added image slider

---
+++ dist/ImagesSlider/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _main = _interopRequireDefault(require("./main"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _default = exports.default = _main.default;
