d8a57056acd558f2cb7fdb65f9f87a8451785af4
NarekBoomTech <nareks@boomte.ch>
2025-02-06 12:27:00 +0400

    update attachments design

--- dist/helpers/commons.js
+++ dist/helpers/commons.js
@@ -122,6 +122,7 @@ async function downloadFile(url, filename) {
     const link = document.createElement("a");
     link.href = blobURL;
     link.download = filename;
+    link.addEventListener("click", event => event.stopPropagation());
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
