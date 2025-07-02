a664a74ff1d0f0a86952f6c95cdd86ca1e433e6e
NarekBoomTech <nareks@boomte.ch>
2025-07-02 17:41:06 +0400

    bump version to 1.9.83 and update ImagesSlider component for autoplay functionality

--- components/src/lib/ImagesSlider/main.js
+++ components/src/lib/ImagesSlider/main.js
@@ -1,59 +1,62 @@
-import React, { memo, useRef, useEffect } from "react";
-import PropTypes from "prop-types";
-import BlurryLoadableImg from "../BlurryLoadableImg";
-import { register } from "swiper/element/bundle";
-import { getRandomNumber } from "../helpers/commons";
-import styles from "./main.module.css";
-import NoImageContainer from "./NoImageContainer";
+import React, { memo, useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import BlurryLoadableImg from '../BlurryLoadableImg';
+import { register } from 'swiper/element/bundle';
+import { getRandomNumber } from '../helpers/commons';
+import styles from './main.module.css';
+import NoImageContainer from './NoImageContainer';
 
 register();
 
 const ImagesSlider = ({
-  image,
-  navigation = true,
-  style,
-  color,
-  title,
-  showColorAsBackground,
-  imgWrapperCustomClassNames = [],
-  imgCustomClassNames,
-  eventKind,
-  opacity,
-  fixedHeight = false,
+	image,
+	navigation = true,
+	style,
+	color,
+	title,
+	showColorAsBackground,
+	imgWrapperCustomClassNames = [],
+	imgCustomClassNames,
+	eventKind,
+	opacity,
+	fixedHeight = false,
+	autoplayToggle = true
 }) => {
-  const swiperRef = useRef();
+	const swiperRef = useRef();
 
-  useEffect(() => {
-    const { current: swiper } = swiperRef;
-    if (!swiper) return;
-    const swiperParams = {
-      on: {
-        init(swiper) {
-          if (!swiper.navigation?.nextEl || !swiper.navigation?.prevEl) return;
-          const stop = (e) => e.stopPropagation();
-          swiper.navigation.nextEl.addEventListener("click", stop);
-          swiper.navigation.prevEl.addEventListener("click", stop);
-        },
-      },
-      lazy: true,
-      autoplay: {
-        delay: getRandomNumber(2000, 4000),
-        pauseOnMouseEnter: true,
-      },
-      speed: "600",
-      loop: "true",
-      navigation,
-      style,
-      ...(fixedHeight ? {} : { autoHeight: true }),
+	useEffect(() => {
+		const { current: swiper } = swiperRef;
+		if (!swiper) return;
+		const swiperParams = {
+			on: {
+				init(swiper) {
+					if (!swiper.navigation?.nextEl || !swiper.navigation?.prevEl) return;
+					const stop = e => e.stopPropagation();
+					swiper.navigation.nextEl.addEventListener('click', stop);
+					swiper.navigation.prevEl.addEventListener('click', stop);
+				}
+			},
+			lazy: true,
+			autoplay: autoplayToggle
+				? {
+						delay: getRandomNumber(2000, 4000),
+						pauseOnMouseEnter: true
+					}
+				: false,
+			speed: '600',
+			loop: 'true',
+			navigation,
+			style,
+			...(fixedHeight ? {} : { autoHeight: true }),
 
-      injectStyles: [
-        `
+			injectStyles: [
+				`
           :host  {
             --swiper-navigation-color: #fff;
             --swiper-navigation-size: 24px;
-            ${fixedHeight ? "height: 100%;width:100%;" : ""}
+            ${fixedHeight ? 'height: 100%;width:100%;' : ''}
           }
-          ${fixedHeight ? "::slotted(swiper-slide) {height: unset;}" : ""}
+          ${fixedHeight ? '::slotted(swiper-slide) {height: unset;}' : ''}
           .swiper-button-next,
           .swiper-button-prev {
             opacity: 0;
@@ -67,57 +70,60 @@ const ImagesSlider = ({
             pointer-events: all;
             transition: .2s;
           }
-          `,
-      ],
-    };
-    Object.assign(swiper, swiperParams);
+          `
+			]
+		};
+		Object.assign(swiper, swiperParams);
 
-    swiper.initialize();
-  }, []);
+		swiper.initialize();
+	}, []);
 
-  return (
-    <swiper-container init="false" ref={swiperRef}>
-      {image.length ? image.map((url) => {
-        return (
-          <swiper-slide>
-            <BlurryLoadableImg
-              url={url}
-              color={color}
-              title={title}
-              wrapperCustomClassNames={[
-                ...imgWrapperCustomClassNames,
-                styles.imageWrapper,
-              ]}
-              imgCustomClassNames={imgCustomClassNames}
-              eventKind={eventKind}
-              opacity={opacity}
-            />
-          </swiper-slide>
-        );
-      }) : <NoImageContainer 
-            color={color} 
-            showColorAsBackground={showColorAsBackground}  
-            wrapperCustomClassNames={[
-              ...imgWrapperCustomClassNames,
-              styles.imageWrapper,
-            ]}  
-        />
-      }
-    </swiper-container>
-  );
+	return (
+		<swiper-container
+			init='false'
+			ref={swiperRef}
+		>
+			{image.length ? (
+				image.map(url => {
+					return (
+						<swiper-slide>
+							<BlurryLoadableImg
+								url={url}
+								color={color}
+								title={title}
+								wrapperCustomClassNames={[
+									...imgWrapperCustomClassNames,
+									styles.imageWrapper
+								]}
+								imgCustomClassNames={imgCustomClassNames}
+								eventKind={eventKind}
+								opacity={opacity}
+							/>
+						</swiper-slide>
+					);
+				})
+			) : (
+				<NoImageContainer
+					color={color}
+					showColorAsBackground={showColorAsBackground}
+					wrapperCustomClassNames={[...imgWrapperCustomClassNames, styles.imageWrapper]}
+				/>
+			)}
+		</swiper-container>
+	);
 };
 
 ImagesSlider.propTypes = {
-  image: PropTypes.arrayOf(PropTypes.string),
-  navigation: PropTypes.bool,
-  color: PropTypes.string,
-  title: PropTypes.string,
-  showColorAsBackground: PropTypes.bool,
-  imgWrapperCustomClassNames: PropTypes.arrayOf(PropTypes.string),
-  imgCustomClassNames: PropTypes.arrayOf(PropTypes.string),
-  eventKind: PropTypes.number,
-  opacity: PropTypes.number,
-  fixedHeight: PropTypes.bool,
+	image: PropTypes.arrayOf(PropTypes.string),
+	navigation: PropTypes.bool,
+	color: PropTypes.string,
+	title: PropTypes.string,
+	showColorAsBackground: PropTypes.bool,
+	imgWrapperCustomClassNames: PropTypes.arrayOf(PropTypes.string),
+	imgCustomClassNames: PropTypes.arrayOf(PropTypes.string),
+	eventKind: PropTypes.number,
+	opacity: PropTypes.number,
+	fixedHeight: PropTypes.bool
 };
 
 export default memo(ImagesSlider);
