040859b3c58bbbe8de45e0e0548fd9458236d682
Boomtech dev <dev@boomte.ch>
2025-10-09 07:22:06 +0000

    modified package-lock.json file

--- dist/AddShareIcons/main.js
+++ dist/AddShareIcons/main.js
@@ -1,7 +1,6 @@
 "use strict";
 
 require("core-js/modules/es.weak-map.js");
-require("core-js/modules/esnext.iterator.constructor.js");
 require("core-js/modules/esnext.iterator.filter.js");
 require("core-js/modules/esnext.iterator.for-each.js");
 Object.defineProperty(exports, "__esModule", {
@@ -9,6 +8,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = AddShareIcons;
 require("core-js/modules/es.array.includes.js");
+require("core-js/modules/esnext.iterator.constructor.js");
 require("core-js/modules/esnext.iterator.map.js");
 require("core-js/modules/web.dom-collections.iterator.js");
 var _react = _interopRequireWildcard(require("react"));
